e97d74a6a362df02ac4421ee9d66b773a93c4568
Christopher Patrick <cpatrick@nabancard.com>
2019-02-14 17:11:55 -0500

    Learning Branch Merging

--- server/server.js
+++ server/server.js
@@ -1,4 +1,13 @@
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Data = require("./data");
+
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+
 
 const app = express();
 
