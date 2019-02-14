81e8e0cc4d27f9306b771e8e0eb3de784a9cec30
Christopher Patrick <cpatrick@nabancard.com>
2019-02-14 17:35:42 -0500

    Work Changes

--- server/server.js
+++ server/server.js
@@ -8,15 +8,53 @@ const API_PORT = 3001;
 const app = express();
 const router = express.Router();
 
+const dbRoute = "mongodb://admin:test123456@ds137255.mlab.com:37255/bookmarks";
 
-const app = express();
+mongoose.connect(
+    dbRoute,
+    { useNewUrlParser: true }
+  );
+  
+  let db = mongoose.connection;
+  
+  db.once("open", () => console.log("connected to the database"));
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Testing Server'
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+  router.get("/getData", (req, res) => {
+    Data.find((err, data) => {
+      if (err) return res.json({ success: false, error: err });
+      return res.json({ success: true, data: data });
     });
-})
+  });
 
-app.listen('5000', () => {
-    console.log('Server listening');
-})
+  router.delete("/deleteData", (req, res) => {
+    const { id } = req.body;
+    Data.findOneAndDelete(id, err => {
+      if (err) return res.send(err);
+      return res.json({ success: true });
+    });
+  });
+
+  router.post("/putData", (req, res) => {
+    let data = new Data();
+  
+    const { id, message } = req.body;
+  
+    if ((!id && id !== 0) || !message) {
+      return res.json({
+        success: false,
+        error: "INVALID INPUTS"
+      });
+    }
+    data.message = message;
+    data.id = id;
+    data.save(err => {
+      if (err) return res.json({ success: false, error: err });
+      return res.json({ success: true });
+    });
+  });
+
+  app.use("/api", router);
+
+  app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
